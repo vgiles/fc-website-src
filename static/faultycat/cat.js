32cf1195560f708a2402635dbc8857881da45469
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2019-10-13 00:01:27 +1100

    tidy up and commit

--- static/faultycat/cat.js
+++ static/faultycat/cat.js
@@ -28,18 +28,6 @@ function mouseClicked() {
         synth.stop();
     }
 }
-// function fc() { // depreciated
-//     for (let i = 0; i < 50; i++) {
-//         let x = int(random(width));
-//         let y = int(random(height));
-//         let col = img.get(x, y);
-//         col = color(red(col), green(col), blue(col), 120);
-//         let size = map(brightness(col), 0, 255, width * 0.01, width * 0.07);
-//         fill(col);
-//         strokeWeight(0.2); 
-//         rect(x, y, size, size);
-//     }
-// }
 
 function draw() {
     //fc();
