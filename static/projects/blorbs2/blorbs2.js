a59ee952a01b12e0e21af59e0e699ff83022a9e5
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2021-10-08 13:52:37 +1100

    adding oscillator and envelope

--- static/projects/blorbs2/blorbs2.js
+++ static/projects/blorbs2/blorbs2.js
@@ -1,8 +1,17 @@
-let sounds = [];
+// let sounds = [];
 let blorb;
 let value;
 let blorbs = [];
+let env;
+let osc;
 let totalBlorbs = 10;
+let icSetMinor = [2, 3, 5, 7, 8, 10];
+let startingNoteSet = [58, 60, 62, 63, 64, 67];
+let t1 = 0.1;
+let l1 = 0.7;
+let t2 = 0.8;
+let l2 = 0.1;
+
 function preload() {
 }
 
@@ -10,37 +19,44 @@ function preload() {
 function setup() {
     createCanvas(720, 400);
     frameRate(25);
-    for (i = 0; i < totalBlorbs; i++) {
+    env = new p5.Envelope(t1, l1, t2, l2);
+    osc = new p5.Oscillator('triangle');
+    for (i = 0; i <= totalBlorbs; i++) {
         let x = random(width);
         let y = random(height);
         let r = random(5, 15);
+        // this could assign a random pitch to each blorb...
         // let randSound = Math.ceil(Math.random() * 10);
         // console.log(randSound);
-        // let s = sounds[randSound];
+        let randStart = Math.floor(Math.random() * startingNoteSet.length);
+        let randInt = Math.floor(Math.random() * icSetMinor.length);
+        let oscFreq = midiToFreq(startingNoteSet[randStart] + icSetMinor[randInt]);
+        let s = osc.freq(oscFreq);
+        env.play(osc);
         blorb = new Blorb(x, y, r);
         blorbs.push(blorb);
     }
+    console.log(blorbs);
 }
 
 function draw() {
     background(220);
-    for (i = 0; i < totalBlorbs; i++)
+    for (i = 0; i <= totalBlorbs; i++)
     {
         blorbs[i].display();
     }
 }
 
 
-// function mousePressed() {
-//     if (getAudioContext().state !== 'running') {
-//         getAudioContext().resume();
-//       }
-// }
+function mousePressed() {
+    if (getAudioContext().state !== 'running') {
+        getAudioContext().resume();
+      }
+}
 function mouseMoved() {
-    for (i = 0; i < totalBlorbs; i++) {
+    for (i = 0; i <= totalBlorbs; i++) {
         blorbs[i].hover(mouseX, mouseY);
-    }
-    
+    } 
 }
 // Constructor
 
@@ -50,7 +66,7 @@ class Blorb {
         this.y = y;
         this.r = r;
         this.f = random(255);
-        // this.s = s;
+        this.s = s;
     }
 
     display() {
@@ -60,7 +76,7 @@ class Blorb {
     }
 
     hover(px, py) {
-        let randNum = random(0, 2);
+        // let randNum = random(0, 2);
         let d = dist(px, py, this.x, this.y);
         if (d < this.r)
         {
@@ -69,6 +85,7 @@ class Blorb {
             this.x = this.x + random(-50, 50);
             this.y = this.y + random(-50, 50);
             // Sound playback per blorb.
+
             // this.s.setVolume(Math.random());
             // if (this.x < width - width/2)
             // {
