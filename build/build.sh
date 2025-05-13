9cd1d368f1a5276e59e076c78551b5456c4db900
vgiles <13183572+vgiles@users.noreply.github.com>
2025-05-13 12:21:58 +1000

    add build script

---
+++ build/build.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# This script is used by Render to build the app for production deployment.
+
+# set up an exit on error
+set -o errexit
+
+# Save the initial directory
+ORIGINAL_DIR="$PWD"
+
+# Install npm dependencies
+# echo "Installing npm dependencies..."
+# npm install --verbose
+
+# Install specific version of Hugo
+HUGO_VERSION="0.147.3"  # Change this to your required version
+echo "Installing Hugo ${HUGO_VERSION}..."
+
+# Create directory for Hugo download and installation
+mkdir -p "${HOME}/bin"
+mkdir -p /tmp/hugo
+cd /tmp/hugo
+
+# Download and install specific Hugo version
+wget -q https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+
+# Move Hugo to a directory you have permission for
+mv hugo "${HOME}/bin/"
+
+# Add the bin directory to PATH
+export PATH="${HOME}/bin:${PATH}"
+
+# Verify installation
+hugo version
+
+# Return to project directory
+cd "$ORIGINAL_DIR"
+
+# Now you can add your Hugo build commands here
+hugo --logLevel info
